3d0c7dd4fcc475f563b663b2d29c64bfa835488b
Akshad-Agarwal <106867353+Akshad-Agarwal@users.noreply.github.com>
2022-07-23 15:36:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+var speechRecognition = window.webkitSpeechRecognition;
+var recongnition = new speechRecognition();
+var synth = window.speechSynthesis;
+var img_id = "";
+
+Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+
+Webcam.attach('#camera');
+
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recongnition.start();
+}
+
+recongnition.onresult = function (event) {
+    console.log(event);
+    content = event.results[0][0].transcript;
+    content = content.toLowerCase();
+    document.getElementById("textbox").innerHTML = content;
+    if (content == "picture activate") {
+        speak_data = "System starting in 5 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+
+
+        setTimeout(function () {
+            img_id = "selfie1";
+            takepicture();
+            speak_data = "Taking your next selfie in 5 seconds";
+            var utterThis = new SpeechSynthesisUtterance(speak_data);
+            synth.speak(utterThis);
+        }, 5000);
+        
+        setTimeout(function () {
+            img_id = "selfie2";
+            takepicture();
+            speak_data = "Taking your next selfie in 5 seconds";
+            var utterThis = new SpeechSynthesisUtterance(speak_data);
+            synth.speak(utterThis);
+        }, 1000);
+
+        setTimeout(function () {
+            img_id = "selfie3";
+            takepicture();
+        }, 15000);
+
+    }
+
+    else {
+        speak_data = "Try again";
+    }
+}
+
+function takepicture() {
+
+    console.log(img_id);
+
+    Webcam.snap(function (data_uri) {
+        if (img_id == "selfie1") {
+            document.getElementById("result1").innerHTML = '<img id="selfie1" src="' + data_uri + '"/>';
+        }
+
+        if (img_id == "selfie2") {
+            document.getElementById("result2").innerHTML = '<img id="selfie2" src="' + data_uri + '"/>';
+        }
+
+        if (img_id == "selfie3") {
+            document.getElementById("result3").innerHTML = '<img id="selfie3" src="' + data_uri + '"/>';
+        }
+    });
+
+    // setTimeout(function () {
+    //     img_id = "selfie1";
+    //     takepicture();
+    //     speak_data = "Taking you next selfie in 10 seconds";
+    //     var utterThis = new SpeechSynthesisUtterance(speak_data);
+    //     synth.speak(utterThis);
+    // }, 5000);
+}
